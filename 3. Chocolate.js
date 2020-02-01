a678ada263c35778b3484df35c430d06358b14aa
SmrithiSrinivaasan <smrithi2006@gmail.com>
2020-02-01 10:31:11 +0530

    Change in Chocolate

--- 3. Chocolate.js
+++ 3. Chocolate.js
@@ -1,5 +1,12 @@
 function choco_break(m,n)
 {
-    return (m*n)-1;
+    if(m>0 && n>0)
+    {
+        return (m*n)-1;
+    }
+    else
+    {
+        return 0; 
+    }
 }
-console.log(choco_break(3,4));
+console.log(choco_break(0,1));
